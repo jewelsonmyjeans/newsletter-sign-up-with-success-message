2fc929f6c983c96ef002693bdfad4f85dc1c55af
Aviad Churaman <aviadchuraman@gmail.com>
2024-02-27 17:46:48 -0500

    Refactor

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const success = document.querySelector("#success");
 const emailInput = document.querySelector("#email");
 
 subscribeBtn.addEventListener("click", () => {
-    const emailValue =  emailInput.value;
-    const reg = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$");
-    if (emailValue === "" || !reg.test(emailValue)) {
-        emailInput.classList.add("errorEmail");
-    } else {
-        mainContent.style.display = "none";
-        success.style.display = "block";
-    }
+  const emailValue = emailInput.value;
+  const reg = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$");
+
+  if (emailValue === "" || !reg.test(emailValue)) {
+    emailInput.classList.add("errorEmail");
+  } else {
+    mainContent.style.display = "none";
+    success.style.display = "block";
+    emailValue = ''
+  }
 });
 
 dismissBtn.addEventListener("click", () => {
